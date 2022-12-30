93b41212351cbfa225e0adf79dc6b5acd43844a5
PRANSHU TANEJA <pranshu45taneja@gmail.com>
2022-12-30 20:49:30 +0530

    added connect to metamask functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,29 +65,32 @@ function App() {
   const [Signer, setSigner] = useState()
   const [Contract, setContract] = useState()
   
-
-  useEffect(() => {             //now this will connect automatically just after page start
-    const connect_metamask = async()=>{
+  const connect_metamask = async()=>{
+    // const connect_metamask = async()=>{
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      // const accounts = await provider.send("eth_requestAccounts", []);
+      const accounts = await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       const contract = new ethers.Contract("0xafB03aafB3dB11AE4470463B9ff3eB6E302Dc20B",abi, Provider)      //Here its the address of contract on the test net blockchain
       // 0x06eCfcee0cAb51decc069b115B187DB6f18C8a44       //ganache local blockchain contract deployed address 
 
-
       setProvider(provider)
       setSigner(signer)
       setContract(contract)
-    }
+      console.log("Setting up providers")
+    // }
+  }
 
+  useEffect(() => {             //now this will connect automatically just after page start
     connect_metamask();
   }, [])
   //------------------- Setting up Provider, Contract & Signer For Functionalities -------------------
 
+  
   return (
     <div className="App">
       {/* adding navbar to the page */}
       <Navbar></Navbar>
+      <button type="button" onClick={connect_metamask}>Connect</button>
       <Routes>
         {/* So basically when you click any routing link then only the area is rendered rather than the whole page that's the advantage tbh */}
           <Route path="/" element={ <Home/> } />        
