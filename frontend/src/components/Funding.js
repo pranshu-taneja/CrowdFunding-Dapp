f00f1cc9480a048e9241818e8c9ff40f43c2b85d
PRANSHU TANEJA <pranshu45taneja@gmail.com>
2023-01-05 22:36:20 +0530

    Final Updates Deployed

--- frontend/src/components/Funding.js
+++ frontend/src/components/Funding.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import './css/funding.css'
 import { ethers } from 'ethers';
@@ -10,6 +10,20 @@ function Funding(props) {
   const DD = location.state.propid
   const Contract = props.Contract;
   const Signer = props.Signer;
+  const Provider = props.Provider;
+
+
+  const [balance, setbalance] = useState(0)
+
+  useEffect(() => {
+    async function fetch_data(){
+      let temp = await Provider.getBalance(DD.acc)
+      temp = Number(ethers.utils.formatEther( temp )).toFixed(2);
+      setbalance(temp);
+    }
+    fetch_data();
+  }, [Provider,DD.acc])
+  
   
 
   const Fund = async () => {            //Fund buttton functionality        
@@ -23,7 +37,6 @@ function Funding(props) {
 
   };  
   
-
   return (
     <div className='FundingProject'>
       <div>
@@ -31,6 +44,7 @@ function Funding(props) {
         <p className='fundingAdd'>Address: {DD.acc}</p>
         <p className='fundingTitle'>Title: {DD.protitle}</p>
         <p className='fundingAmount'>Amount Needed: {DD.amount} ETH</p>
+        <p className='RaisedAmount'>Amount Raised: {balance} ETH</p>
         <p className='fundingDesc'>Description: {DD.projdesc}</p>
         <p className='fundingExp'>Expiry: {DD.expdate}</p>
         <input className='fundingInput' placeholder='Enter Amount Here' type="text"/>
@@ -39,6 +53,7 @@ function Funding(props) {
       <div className="fundingAmount">
         <button className='fundingButton' type='submit' onClick={Fund}>Fund</button>
       </div>
+
     </div>
   )
 }
